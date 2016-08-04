4573562dad3da4715637a88c4a37b06983a2c077
HandicapableDog <conner@accuratefishing.com>
2016-08-04 13:47:08 -0400

    Loop Function
    Completed loop function and added beginning number security

--- isPrime.py
+++ isPrime.py
@@ -55,7 +55,6 @@ def count(chk):
 			else:
 				print "Try again"
 					
-					
 def solver(chk, intType):
 	#intType defines whether or not lists will be recreated, for types 2 and 3 only
 	listPos = 0
@@ -80,27 +79,80 @@ def solver(chk, intType):
 	elif (intType == 3):
 		count(chk)
 	# Make array to save each answer, .find(0) later
-		
 	# listPos must update opposite cycleNumber in order to accurately assign numbers to the list
+
 def checker():
 	intType = 1
 	chk = input("Input number to be checked here: ")
 	solver(chk,intType)
-		
-		
-loop(chk, loopNum):
-		
 
-		
+def loop(chk, loopNum):
+	cont = True
+	while (cont == True):
+		loopList = [0];
+		del loopList[0]
+		prime = 0
+		print "Number : Modulus" 
+		while (prime == 0):
+			listPos = 0
+			output = 0
+			cycleNum = chk - 1
+			ls = [1];
+			while (cycleNum > 0):
+				output = chk%cycleNum
+				ls.append(output)
+				print str(cycleNum + 1) + " : " + str(ls[listPos])
+				listPos += 1
+				cycleNum -= 1
+			del ls[-1]
+			if (0 in ls):
+				print str(chk) + " is not a prime"
+				print "Checking" + " " + str(chk + 1)
+				prime = 0
+			else:
+				print str(chk) + " is a prime"
+				print "Checking" + " " + str(chk + 1)
+				loopList.append(chk)
+				if (len(loopList) == loopNum):
+					prime = 1
+					print "These are the %d requested prime numbers " %(loopNum) + str(loopList)
+				else:
+					prime = 0
+			chk += 1
+		incorrect = 1
+		while (incorrect == 1):
+			cont = raw_input("Would you like to continue onto %d (t/f)?" %(chk))
+			if (cont == "t"):
+				cont = True
+				incorrect = 0
+				prime = 0
+			elif (cont == "f"):
+				cont = False
+				incorrect = 0
+				print "Thank you!"
+			else:
+				print "Try again"
+
 def countUp(firstInput):
 	if (firstInput == 2):
 		solver(2, 2)
 	elif (firstInput == 4):
-		chk = raw_input("Insert number to be started from here: ")
-		loopNum = raw_input("Insert number of loops between check: ")
-		loop(chk, loopNum)
+		correct = 0
+		while (correct == 0):
+			chk = input("Insert number to be started from here: ")
+			loopNum = input("Insert number of loops between check: ")
+			if (chk >= 2):
+				if (loopNum <= 500):
+					loop(chk, loopNum)
+					correct = 1
+				else:
+					print "Number of loops cannot be greater than 500"
+			else:
+				print "Number must be greater than 2"
+				correct = 0
 	else:
 		intType = 3
 		chk = input("Insert number to be started from here: ")
 		solver(chk, intType)
+
 start()
