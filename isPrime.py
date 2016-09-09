87c5715d523ec932527a7186e1b865c11108702d
HandicapableDog <conner@accuratefishing.com>
2016-09-08 18:16:58 -0700

    Speed Changes
    Added a new speed function which does not display mods.

--- isPrime.py
+++ isPrime.py
@@ -1,16 +1,16 @@
 import time
 def start():
-	firstInput = raw_input("1: Check Number\n2: Count Up From Number\n3: Count Up From 2\n4: Loop from Number\nInput Here: ")
+	firstInput = raw_input("1: Check Number\n2: Loop from Number\n3: Speed Check\n4: Speed Loop from Number\nInput Here: ")
 	if (firstInput == "1"):
 		firstInput = int(firstInput)
 		checker()
-	elif (firstInput == "2"):
+	elif (firstInput == "4"):
 		firstInput = int(firstInput)
 		countUp(firstInput)
 	elif (firstInput == "3"):
 		firstInput = int(firstInput)
 		countUp(firstInput)
-	elif (firstInput == "4"):
+	elif (firstInput == "2"):
 		firstInput = int(firstInput)
 		countUp(firstInput)
 	else:
@@ -22,6 +22,7 @@ def count(chk):
 	while (cont == True):
 		prime = 0
 		print "Number : Modulus" 
+		time.sleep(1.5)
 		while (prime == 0):
 			listPos = 0
 			output = 0
@@ -67,6 +68,7 @@ def solver(chk, intType):
 	if (intType == 1):
 		ls = [1];
 		print "Number : Modulus"
+		time.sleep(1.5)
 		while (cycleNum > 0):
 			output = chk%cycleNum
 			ls.append(output)
@@ -103,6 +105,7 @@ def loop(chk, loopNum):
 		del loopList[0]
 		prime = 0
 		print "Number : Modulus" 
+		time.sleep(1.5)
 		while (prime == 0):
 			listPos = 0
 			output = 0
@@ -144,33 +147,77 @@ def loop(chk, loopNum):
 			else:
 				print "Try again"
 
+def divider(chk, chkNum, chkInt, start):
+    found = 0
+    while (found == 0):
+        output = 0
+        cycleNum = chkNum - 1
+        cycleNum = cycleNum/2
+        nPrime = 0
+        while (nPrime == 0):
+            if (chkInt == 0):
+                cycleNum -= 1
+            elif (chkInt == 5):
+                cycleNum -= 1
+            elif (chkInt % 2 == 0):
+                cycleNum -= 1
+            else:
+                output = chkNum%cycleNum
+                if (output == 0):
+                    nPrime = 1
+                elif (cycleNum == 2):
+                	nprime = 2
+                else:
+                    cycleNum -= 1
+        if (nPrime == 1):
+            print str(chk) + " is not a Prime"
+            found = 1
+        elif (nprime == 2):
+            print str(chk) + " is a Prime"
+            found = 1
+        print "Completed in: " + str(time.clock() - start) + " seconds"
+
+def nsolve(chk, chkNum):
+    start = time.clock()
+    chkLast = int(chk[-1])
+    if (chkLast == 5):
+        print "Not a Prime"
+        print "Completed in: " + str(time.clock() - start) + " seconds"
+    elif (chkLast % 2 == 0):
+        print "Not a Prime"
+        print "Completed in: " + str(time.clock() - start) + " seconds"
+    elif (chkLast == 0):
+        print "Not a Prime"
+        print "Completed in: " + str(time.clock() - start) + " seconds"
+    else:
+        divider(chk, chkNum, chkLast, start)
+
 def countUp(firstInput):
 	if (firstInput == 2):
-		solver(2, 2)
-	elif (firstInput == 4):
 		correct = 0
 		while (correct == 0):
 			chk = input("Insert number to be started from here: ")
-			if (chk >= 2):
+			if (chk > 2):
 				loopNum = input("Insert number of loops between check: ")
-				if (loopNum <= 500):
+				if (loopNum <= 5000):
 					if (loopNum >= 2):
 						loop(chk, loopNum)
 						correct = 1
 					else:
 						print "Number of loops must be greater than 1"
 				else:
-					print "Number of loops cannot be greater than 500"
+					print "Number of loops cannot be greater than 5000"
 			else:
-				print "Number must be greater than 1"
+				print "Number must be greater than 2"
 				correct = 0
-	else:
-		intType = 3
-		chk = input("Insert number to be started from here: ")
-		if (chk >= 2):	
-			solver(chk, intType)
+	elif (firstInput == 3):
+		chk = raw_input("Input number to be checked here: ")
+		chkNum = int(chk)
+		if (chkNum > 2):
+		    nsolve(chk, chkNum)
 		else:
-			print "Number must be greater than 1"
-			countUp(firstInput)
+		    print "Number must be greater than 1"
+		    countup(3)
+#	else:
 
 start()
